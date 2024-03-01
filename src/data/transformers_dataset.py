04e54f968a8b5b2b89c15891c8d40148aeaf1a0c
Joel <2944355033@qq.com>
2024-03-01 16:46:04 +0800

    Update transformers_dataset.py

--- src/data/transformers_dataset.py
+++ src/data/transformers_dataset.py
@@ -34,6 +34,8 @@ class TransformersNERDataset(Dataset):
         self.parser_mode = parser_mode
         self.dep_mode = dep_model
         self.max_entity_length = 0
+        self.entity_length_counts = {}
+        self.total_entities = 0
         self.insts = self.read_from_json(file=file) if is_json else self.read_file(file=file, number=number)
         # self.insts = self.read_file(file=file, number=number) if sents is None else self.read_from_sentences(sents)
         # minus = int((self.max_entity_length + 1) * self.max_entity_length / 2)
@@ -163,21 +165,21 @@ class TransformersNERDataset(Dataset):
         return features
 
     def read_from_json_clue(self, file:str)-> List[Instance]:
-    print(f"[Data Info] Reading file: {file}")
-    insts = []
-    with open(file, 'r', encoding='utf-8') as file:
-        for line in file:
-            data = json.loads(line)
-            chunks = []
-            words = data["text"]
-            for entity_type, entity_data in data["label"].items():
-                for _, span in entity_data.items():
-                    chunks.append(((span[0][0], span[0][1]), entity_type))
-                    chunks_len = span[0][1] - span[0][0]
-                    self.entity_length_counts[chunks_len] = self.entity_length_counts.get(chunks_len, 0) + 1
-                    self.total_entities += 1
-            insts.append(Instance(words=words, ori_words=words, dep_heads=None, dep_labels=None, span_labels=chunks, labels=None))
-    return insts
+        print(f"[Data Info] Reading file: {file}")
+        insts = []
+        with open(file, 'r', encoding='utf-8') as file:
+            for line in file:
+                data = json.loads(line)
+                chunks = []
+                words = data["text"]
+                for entity_type, entity_data in data["label"].items():
+                    for _, span in entity_data.items():
+                        chunks.append(((span[0][0], span[0][1]), entity_type))
+                        chunks_len = span[0][1] - span[0][0]
+                        self.entity_length_counts[chunks_len] = self.entity_length_counts.get(chunks_len, 0) + 1
+                        self.total_entities += 1
+                insts.append(Instance(words=words, ori_words=words, dep_heads=None, dep_labels=None, span_labels=chunks, labels=None))
+        return insts
                                                  
     def read_from_json(self, file: str)-> List[Instance]:
         print(f"[Data Info] Reading file: {file}")
